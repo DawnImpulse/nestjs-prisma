2b3b9ccb979fd587299140630a17e135fa67a770
Saksham <dawnimpulse@gmail.com>
2026-01-06 14:14:40 +0530

    feat: Implement API key authentication middleware for user routes and add Swagger API key definition.

--- dist/src/user/entities/user.entity.d.ts
+++
@@ -1,7 +0,0 @@
-import { User as PrismaUser } from '@prisma/client';
-export declare class User implements PrismaUser {
-    id: string;
-    name: string;
-    createdAt: Date;
-    updatedAt: Date;
-}
